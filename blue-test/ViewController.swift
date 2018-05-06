c079d62bd5b41d831d6b6bcfa50c4927fb43400a
celineselee <celineselee@gmail.com>
2018-05-05 20:37:34 -0400

    Add files via upload

---
+++ blue-test/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  blue-test
+//
+//  Created by Celine Lee on 2018-05-05.
+//  Copyright © 2018 Celine Lee. All rights reserved.
+//
+
+import UIKit
+import NaturalLanguageUnderstandingV1
+
+class ViewController: UIViewController {
+    @IBAction func checkButton(_ sender: Any) {
+//        NSLog(textField.text!)
+        
+        textLabel.text = textField.text
+        
+        let nls = NaturalLanguageUnderstanding(username: "3c125aaa-6f52-4791-ba2f-214ebc8dd988", password: "ERdsqN5Sqdk6", version: "2018-03-16");
+        
+        let features = Features(concepts: nil, emotion: nil, entities: nil, keywords: nil, metadata: nil, relations: nil, semanticRoles: nil, sentiment: SentimentOptions(document: true, targets: nil), categories: nil);
+        
+        let params = Parameters(features: features, text: textField.text, html: nil, url: nil, clean: nil, xpath: nil, fallbackToRaw: nil, returnAnalyzedText: true, language: nil, limitTextCharacters: nil);
+        
+        
+          nls.analyze(parameters: params, success:{ results in
+            self.printSentiment(sentimentResult: results.sentiment.unsafelyUnwrapped.document?.label as! String);});
+        
+//        nls.analyze(parameters: params, success:{ results in
+//            print(results.sentiment.unsafelyUnwrapped.document?.label);
+//        });
+
+//        nls.analyze(parameters: params, success:{ results in
+//            print("%@", results);
+//        });
+    }
+    
+    func printSentiment(sentimentResult: String) {
+        if sentimentResult == "negative" {
+            sadSentiment();
+        }
+        else {
+            happySentiment();
+        }
+    }
+    
+    func sadSentiment (){
+        print("Sad");
+    }
+    
+    func happySentiment (){
+        print("Happy");
+    }
+    
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
